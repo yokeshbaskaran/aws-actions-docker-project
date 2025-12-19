030c2bb0e66ec22c0caf9f1ec89e311a7c2e6e01
Yokesh <yokeshbaskaran@gmail.com>
2025-12-19 17:56:04 +0530

    installed packages, added routes and models (schema) -all done  in backend and database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,101 @@
+require("dotenv").config({ quiet: true });
+
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ messages: "GET route" });
+app.use(express.json());
+
+// // dummy data
+// const todos = [];
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log("DB connection failed!");
+  });
+
+// creating TODO schema
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+const Todo = mongoose.model("todo", todoSchema);
+
+// Routes
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (error) {
+    console.log("GET TODO failed!");
+  }
+});
+
+app.post("/todos", async (req, res) => {
+  const { title } = req.body;
+  //   const newTodo = {
+  //     id: todos.length + 1,
+  //     title: title,
+  //   };
+  try {
+    const newTodo = new Todo({ title });
+    await newTodo.save();
+
+    console.log(newTodo);
+    res.status(201).json({ newTodo, message: "Todo created" });
+  } catch (error) {
+    console.log("POST Todo failed!");
+  }
+});
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const singleTodo = await Todo.findById(id);
+    res.status(200).json(singleTodo);
+  } catch (error) {
+    console.log("GET SINGLE Todo failed!");
+  }
+});
+
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { title } = req.body;
+    const id = req.params.id;
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      id,
+      { title },
+      { new: true }
+    );
+
+    if (!updatedTodo) {
+      return res.status(404).json({ message: "Todo not found!" });
+    }
+
+    res.status(200).json(updatedTodo);
+  } catch (error) {
+    console.log("DELETE Todo failed!");
+  }
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Todo.findByIdAndDelete(id);
+    res.status(200).json({ message: `Todo deleted` });
+  } catch (error) {
+    console.log("DELETE Todo failed!");
+  }
 });
 
 //server on port
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
