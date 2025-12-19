cd13b287aa0a33906bfa961804cd2d85e1931b33
Yokesh <yokeshbaskaran@gmail.com>
2025-12-19 18:04:47 +0530

    installed tailwind css and setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="m-10 p-10">
+        <h2 className="text-xl text-blue-500">Tailwind is working!</h2>
+      </div>
+    </>
+  );
 };
 
 export default App;
