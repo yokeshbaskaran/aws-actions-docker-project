d30a1606c7a65dfa924ba5b0c8f83f7bc5a82b06
Yokesh <yokeshbaskaran@gmail.com>
2025-12-19 15:41:01 +0530

    backend setup done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Express is here!");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ messages: "GET route" });
+});
+
+//server on port
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
